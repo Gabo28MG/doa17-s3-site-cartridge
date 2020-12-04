5dcc18f4038bb359f230e62e93fd77f58a286487
Gabo28MG <74741265+Gabo28MG@users.noreply.github.com>
2020-12-04 15:39:14 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# doa17-s3-site-cartridge
-Copy Cartucho 1
+# ADOP S3 Site Cartridge Pipeline
+
+This cartridge creates an S3 Bucket and deploy a static page on it.
+
+### Step 1
+Open your github account and fork these repositories:
+- https://github.com/chuymarin/doa17-s3-site-cartridge
+- https://github.com/chuymarin/doa17-static-page
+
+Note: A fork is a copy of a repository to another account
+
+### Step 2
+Open the file: doa17-s3-site-cartridge/jenkins/jobs/dsl/doa17_s3_site_cartridge.groovy
+- Change the value of the variable doa17BucketName = "doa17-{your-github-user}-bucket"
+- Example: doa17-chuymarin-bucket
+- Add, Commit and Push your changes
+
+### Step 3
+Open the file: doa17-static-page/index.html
+- Search where it says: {name} and change it with your github user name
+- Add, Commit and Push your changes
+
+### Step 4
+Open ADOP url (will be provided by your instructors)
+- Open Jenkins
+- Go to DevOps_Academy Workspace
+- Create a Project (Name: your github user)
+
+### Step 5
+Loading Cartridge
+- In your created project, open load_cartrdige job
+- Fill the values and execute the job
+- It should create a new directory, some jobs and views
+
+### Step 6
+Execute the pipeline
+- Open your created view and execute the pipeline
+- If it works, check the log of the last job and copy the url
+- Open a web browser and paste that url to see your new page
+
+### Good Job!
+
+
---
+++ jenkins/jobs/dsl/doa17_s3_site_cartridge.groovy
@@ -0,0 +1,164 @@
+// Folders
+def workspaceFolderName = "${WORKSPACE_NAME}"
+def projectFolderName = "${PROJECT_NAME}"
+
+// Variables
+def siteRepoName = "doa17-static-page"
+def siteRepoUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/" + siteRepoName
+def doa17BucketName = "doa17-chuymarin-bucket"
+
+// Views
+def doa17SitePipeline = buildPipelineView(projectFolderName + "/DOA17_S3_Site_Pipeline")
+
+// Jobs
+def doa17PullCode = freeStyleJob(projectFolderName + "/DOA17_Pull_Code")
+def doa17CreateBucket = freeStyleJob(projectFolderName + "/DOA17_Create_Bucket")
+def doa17DeploySite = freeStyleJob(projectFolderName + "/DOA17_Deploy_Site")
+
+
+// DOA17_S3_Site_Pipeline
+doa17SitePipeline.with{
+    title('DOA17 S3 Site Pipeline')
+    displayedBuilds(5)
+    selectedJob(projectFolderName + "/DOA17_Pull_Code")
+    showPipelineDefinitionHeader()
+    alwaysAllowManualTrigger()
+    refreshFrequency(5)
+}
+
+// DOA17_Pull_Code
+doa17PullCode.with{
+  description("Clones Static Page Repo")
+  environmentVariables {
+    env('WORKSPACE_NAME', workspaceFolderName)
+    env('PROJECT_NAME', projectFolderName)
+  }
+  parameters{
+    stringParam("S3_BUCKET",doa17BucketName,"AWS S3 Bucket Name")
+    stringParam("AWS_REGION",'',"AWS Region")
+  }
+  wrappers {
+    preBuildCleanup()
+    maskPasswords()
+    sshAgent("adop-jenkins-master")
+  }
+  label("docker")
+  triggers {
+   gerrit {
+     events {
+       refUpdated()
+     }
+     project("${PROJECT_NAME}/" + siteRepoName, 'master')
+    }
+  }
+  scm{
+     git{
+       remote{
+         url(siteRepoUrl)
+         credentials("adop-jenkins-master")
+       }
+       branch("*/master")
+     }
+  }
+  publishers{
+    archiveArtifacts("**/*")
+    downstreamParameterized{
+      trigger(projectFolderName + "/DOA17_Create_Bucket"){
+        condition("UNSTABLE_OR_BETTER")
+        parameters{
+          currentBuild()
+        }
+      }
+    }
+  }
+}
+
+// DOA17_Create_Bucket
+doa17CreateBucket.with{
+  description("Create AWS S3 Bucket")
+  environmentVariables {
+    env('WORKSPACE_NAME', workspaceFolderName)
+    env('PROJECT_NAME', projectFolderName)
+  }
+  parameters{
+    stringParam("S3_BUCKET",'',"AWS S3 Bucket Name")
+    stringParam("AWS_REGION",'',"AWS Region")
+  }
+  wrappers {
+    preBuildCleanup()
+    maskPasswords()
+  }
+  label("docker")
+    steps {
+    shell('''
+set +x
+
+echo "[LOG] Export AWS Configuration"
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+
+echo "[LOG] Create S3 Bucket $S3_BUCKET if not exists"
+S3_BUCKETS=$(aws s3 ls)
+if grep -q $S3_BUCKET <<<$S3_BUCKETS; then
+  echo "[LOG] S3 Bucket $S3_BUCKET already exists"
+else
+  echo "[LOG] Creating S3 Bucket $S3_BUCKET"
+  aws s3 mb s3://$S3_BUCKET
+fi
+
+set -x'''.stripMargin()
+    )
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(projectFolderName + "/DOA17_Deploy_Site"){
+        condition("UNSTABLE_OR_BETTER")
+        parameters{
+          currentBuild()
+        }
+      }
+    }
+  }
+}
+
+// DOA17_Deploy_Site
+doa17DeploySite.with{
+  description("Deploy Site to AWS S3")
+  environmentVariables {
+    env('WORKSPACE_NAME', workspaceFolderName)
+    env('PROJECT_NAME', projectFolderName)
+  }
+  parameters{
+    stringParam("S3_BUCKET",'',"AWS S3 Bucket Name")
+    stringParam("AWS_REGION",'',"AWS Region")
+  }
+  wrappers {
+    preBuildCleanup()
+    maskPasswords()
+  }
+  label("docker")
+  steps {
+    copyArtifacts(projectFolderName + "/DOA17_Pull_Code") {
+      buildSelector {
+        latestSuccessful(true)
+      }
+    }
+    shell('''
+set +x
+
+echo "[LOG] Export AWS Configuration"
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+echo "[LOG] Deploy Site to AWS S3 Bucket"
+aws s3 cp index.html s3://$S3_BUCKET/index.html --acl public-read
+
+echo "[LOG] Create Website from S3 Bucket"
+aws s3 website s3://$S3_BUCKET/ --index-document index.html
+
+echo "[LOG] Check your new site at:"
+echo "http://$S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com"
+
+set -x'''.stripMargin()
+    )
+  }
+}
